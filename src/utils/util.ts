c6cc3635cf75d3c732b4ac66b5203bc9b17a9ead
frience0 <marckfrience0@gmail.com>
2025-08-26 14:45:45 +0800

    next config fix

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,7 +1,2 @@
-const getImagePrefix = () => {
-    return process.env.NODE_ENV === "production"
-        ? "/E-learning/"
-        : "";
-};
-
+const getImagePrefix = () => "";
 export { getImagePrefix };
